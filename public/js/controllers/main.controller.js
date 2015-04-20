8a1571831a70f5a6c97d5b4f66326dbcc24eb477
Yin Zhao <kidzhaoyin@gmail.com>
2015-04-20 19:11:33 -0400

    modified getCategory functions in main controller

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -1,13 +1,15 @@
-app.controller('MainController', function ($scope, FlashCardsFactory) {
-	$scope.flashCards = FlashCards.getFlashCards();
-
+app.controller('MainController', function ($scope, FlashCardsFactory, ScoreFactory) {
+	$scope.getAllCards = function() {
+	  $scope.currentCategory = null;
+	  $scope.flashCards = FlashCards.getFlashCards();
+	  
 	$scope.answerQuestion = function (answer, flashCard) {
 		if (!flashCard.answered) {
 			flashCard.answered = true;
 			flashCard.answeredCorrectly = answer.correct;
 		}
 	}
-
+	};
 	$scope.categories = [
 	  'MongoDB',
 	  'Express',
@@ -16,6 +18,10 @@ app.controller('MainController', function ($scope, FlashCardsFactory) {
 	];
 
 	$scope.getCategoryCards = function(category){
-
-	};
+		FlashCardsFactory.getFlashCards(category).then(function(cards){
+			$scope.flashCards = cards;
+			$scope.currentCategory = category;
+	
+	});
+	}
 });
