455d81973992e8f773ab793b40a8993b25507f31
Yin Zhao <kidzhaoyin@gmail.com>
2015-04-20 10:31:15 -0400

    category and button

--- public/app.js
+++ public/app.js
@@ -26,3 +26,20 @@ app.value('whateverName', [
         ]
     }
 ]);
+
+app.factory('FlashCardsFactory', function ($http) {
+    return { 
+	getFlashCards: function(category){
+	  var queryParams = {};
+	  if (category) {
+	    queryParams.category = category;
+	  }
+	  return $http.get('/cards', {
+		params: queryParams
+	  }).then(function(response) {
+	    return response.data;
+	  });
+	} 
+    };
+});
+
