7df98cd301b089866c39a7f15556703a249133e6
Yin Zhao <kidzhaoyin@gmail.com>
2015-05-12 16:34:56 -0400

    modified flash-card.js

--- public/js/directives/flash-card/flash-card.js
+++ public/js/directives/flash-card/flash-card.js
@@ -1,6 +1,11 @@
 app.directive('flash-card', function () {
     return {
-	restrict: 'E'
+	restrict: 'E',
+	scope: {
+	  card: '=',
+	  answerFunction: '='
+	},
+	templateUrl: 'js/directives/flash-card/flash-card.html'
     };
 
 
